e99049ed3a601d49c6213662e17bc85baa34c2cb
Julie Hartz <julie.hartz13@gmail.com>
2021-06-15 14:52:36 +0200

    Added label_data function

--- SafeSeizure/data.py
+++ SafeSeizure/data.py
@@ -32,3 +32,36 @@ def get_data():
             array = decimate(decimate(data[segment_name][0][0][0],10),10) 
             dataframe = pd.DataFrame(array)
             dataframe.to_csv(f'{dir}_csv/{segment_name}.csv', index = False)
+
+def label_data():
+    '''Function that adds a target (0: interictal, 1:preictal) to each csv file and exports
+    as a new *_labelled.csv file'''
+    files_1 = []
+    files_2 = []
+
+    dirs = ['Patient_1_csv', 'Patient_2_csv']
+    for f in os.listdir(os.path.join(FOLDER_PATH,dirs[0],'train_segments_unlabelled')):
+        files_1.append(f)
+        
+    for f in os.listdir(os.path.join(FOLDER_PATH,dirs[1],'train_segments_unlabelled')):
+        files_2.append(f)
+
+    for f in files_1:
+        if f.startswith('interictal'):
+            target = 0
+        else:
+            target = 1
+        data = pd.read_csv(f'{FOLDER_PATH}/{dirs[0]}/train_segments_unlabelled/{f}')
+        data['target'] = target
+        f_labelled = f.strip('.csv')
+        data.to_csv(f'{FOLDER_PATH}/{dirs[0]}/train_segments_labelled/{f_labelled}_labelled.csv', index = False)
+    
+    for f in files_2:
+        if f.startswith('interictal'):
+            target = 0
+        else:
+            target = 1
+        data = pd.read_csv(f'{FOLDER_PATH}/{dirs[1]}/train_segments_unlabelled/{f}')
+        data['target'] = 1
+        f_labelled = f.strip('.csv')
+        data.to_csv(f'{FOLDER_PATH}/{dirs[1]}/train_segments_labelled/{f_labelled}_labelled.csv', index = False)
